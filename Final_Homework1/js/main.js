e4a6c90114a47821623f2008b25c26af40f6d0b9
boloramg <44391126+boloramg@users.noreply.github.com>
2019-04-25 01:35:53 -0400

    Final_HW1_Process videos added

--- Final_Homework1/js/main.js
+++ Final_Homework1/js/main.js
@@ -1,17 +1,29 @@
-var http = require('http');
+//1.
+
 const Clarifai = require('clarifai');
 
 // Instantiate a new Clarifai app by passing in your API key.
+const app = new Clarifai.App({apiKey: 'f17933226feb4ddea3e8c260b9fc428c'});
 
-http.createServer(function (req, app) {
-	const app = new Clarifai.App({apiKey: 'f17933226feb4ddea3e8c260b9fc428c'});
+// Predict the contents of an image by passing in a URL.
+app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
+  .then(response => {
+    console.log(response);
+    console.log("Hello, this is my own custom message to show with the response to this call.");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
-	// Predict the contents of an image by passing in a URL.
-	app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg')
-	  .then(response => {
-	    console.log(response);
-	  })
-	  .catch(err => {
-	    console.log(err);
-	  });
-}).listen(8080);
+//2.
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+
+//NOT SURE HOW TO COMBINE 1 AND 2. TRIED A FEW DIFFERENT WAYS, BUT NEED MORE TIME TO REALLY UNDERSTAND.
+//Problem is that 'require' at line 3 is not a known method when Node.js is not initialized. But the only 
+//way I know how to initialize Node.js is through the command prompt interface, and this initialization
+//is not connected to the HTML file...???
